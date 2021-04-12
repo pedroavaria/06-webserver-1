894d031e4a9d02c8d1aea697c5fc1a6f92c64528
Pedro Avaria <pavaria@mesos.cl>
2021-04-12 15:06:05 -0400

    Despliegue app - 2021-03-12

--- src/app.ts
+++ src/app.ts
@@ -5,38 +5,38 @@ const app = express()
 const port = process.env.PORT
 
 // Handlebas
-app.set('view engine','hbs')
-hbs.registerPartials(__dirname + '/../views/partials');
+// app.set('view engine','hbs')
+// hbs.registerPartials(__dirname + '/../views/partials');
 
 // Servir contenido estático
 app.use(express.static('public'))
 
-const datos = {
-    nombre: 'Pedro Avaria',
-    titulo: 'Curso de node'
-}
+// const datos = {
+//     nombre: 'Pedro Avaria',
+//     titulo: 'Curso de node'
+// }
 
-app.get('/', (req,res) => {
-    res.render('home',datos)
-})
+// app.get('/', (req,res) => {
+//     res.render('home',datos)
+// })
 
-app.get('/hola-mundo', (req,res) => {
-    res.send('Hola que tal')
-})
+// app.get('/hola-mundo', (req,res) => {
+//     res.send('Hola que tal')
+// })
 
-app.get('/generic', (req,res) => {
-    res.render('generic',datos)
-})
+// app.get('/generic', (req,res) => {
+//     res.render('generic',datos)
+// })
 
-app.get('/elements', (req,res) => {
-    res.render('elements',datos)
-})
+// app.get('/elements', (req,res) => {
+//     res.render('elements',datos)
+// })
 
 app.get('*', (req,res) => {
-    res.sendFile(__dirname + '/../public/404.html')
+    res.sendFile(__dirname + '/../public/index.html')
 })
 
 app.listen(port, () => {
-    console.log(`Eñ servidor esta escuchando en http://localhost:${port}`);
+    console.log(`El servidor esta escuchando en http://localhost:${port}`);
     
 })
